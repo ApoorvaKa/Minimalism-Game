efdfdc27e90b60c58fe2448ad64e39c6abc2088d
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-01-31 16:21:11 -0500

    player and infinite scroll compiled in main

--- Week1 copy/src/Actors/Player.gd
+++ Week1 copy/src/Actors/Player.gd
@@ -1,46 +1,22 @@
-extends Actor
-
-func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
-	queue_free()
+extends Area2D
+signal hit
 
 var screen_size
 
-func _ready(): 
+# Called when the node enters the scene tree for the first time.
+func _ready():
 	screen_size = get_viewport_rect().size
-	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("gravity_flip"):
 		if position.y > 500:
-			position.y = 145
+			position.y = 73
 		else:
-			position.y = 560
-
-#func _physics_process(delta: float) -> void:
-#	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
-#	var direction: = get_direction()
-#	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-#	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-
-
-#func get_direction() -> Vector2:
-#	return Vector2(
-#		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-#		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
-#	)
+			position.y = 532
 	
-#func calculate_move_velocity(
-#	linear_velocity: Vector2,
-#	direction: Vector2,
-#	speed: Vector2,
-#	is_jump_interrupted: bool
-#	) -> Vector2:
-#	var new_velocity: = linear_velocity
-#	new_velocity.x = speed.x * direction.x
-#	new_velocity.y += gravity * get_physics_process_delta_time()	
-#
-#	if direction.y == -1.0:
-#		new_velocity.y = speed.y * direction.y
-#	if is_jump_interrupted:
-#		new_velocity.y = 0.0
-#	return new_velocity
 
+func _on_Area2D_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
