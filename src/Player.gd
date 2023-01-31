3a51a85cbaa8a30c06e92bfd72a8cb824fbfe539
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-01-30 20:39:46 -0500

    player antigravity with space bar

--- src/Player.gd
+++ src/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var speed = 200 
+export var speed = 350 
 var screen_size
 var power = 0
 # Called when the node enters the scene tree for the first time.
@@ -10,19 +10,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_just_released("click"):
-		velocity.y -= power;
-		power = 0
-	if Input.is_action_pressed("click"):
-		power += 1
-		
+	var velocity = Vector2.ZERO # set velocity to (0,0)
+	if Input.is_action_just_pressed("gravity_flip"):
+		if position.y > 500:
+			position.y = 135
+		else:
+			position.y = 475
 	
+	velocity.x += 1
+		
 	if velocity.length() > 0:
-		velocity = velocity * speed
+		velocity = velocity.normalized() * speed
+		
 	# handles the movement of the player
 	position += velocity * delta
-	
 	position.x = clamp(position.x, 0, screen_size.x) # clamp to keep it within the screen
 	position.y = clamp(position.y, 0, screen_size.y)
 	
