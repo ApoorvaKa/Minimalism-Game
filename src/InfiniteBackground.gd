78887945f1010a4334b23891f593fe88b8db9365
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-01-30 20:16:04 -0500

    background mirroring and layout

--- src/InfiniteBackground.gd
+++ src/InfiniteBackground.gd
@@ -1,6 +1,6 @@
 extends ParallaxBackground
 
-export (float) var scrolling_speed = 500
+export (float) var scrolling_speed = 350
 
 func _process(delta):
 	scroll_offset.x -= scrolling_speed * delta
