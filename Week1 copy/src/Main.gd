d2e70a8d3a08a637a586a4db3e6e30e18153c3e4
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-01-31 17:23:39 -0500

    Initial HUD Implementation

--- Week1 copy/src/Main.gd
+++ Week1 copy/src/Main.gd
@@ -4,7 +4,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game() # will be removed with HUD implementation
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -12,13 +12,18 @@ func _ready():
 
 func game_over():
 	$ScoreTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$ScoreTimer.start()
